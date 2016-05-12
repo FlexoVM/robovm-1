8be918c495429b79af2842f74c7c466c23f00962
Erik De Rijcke <derijcke.erik@gmail.com>
2016-05-12 21:18:27 +0200

    delete copied runtime as we will not work on it yet, begin adding cmake files with a list of TODO's in the relevant directories

--- compiler/vm/core-jdk8-compact1/include/robovm/shadowframes.h
+++
@@ -1,8 +0,0 @@
-#ifndef ROBOVM_SHADOW_FRAMES_H
-#define ROBOVM_SHADOW_FRAMES_H
-
-void rvmPushShadowFrame(Env* env, ShadowFrame* frame);
-void rvmPopShadowFrame(Env* env);
-void rvmPushShadowFrameLineNumber(ShadowFrame* frame, jint lineNumber);
-
-#endif
